ef1010a0328299a36f548e13cd04bcbbce60897b
wusagi24 <lapin.wuusan@gmail.com>
2016-08-14 05:02:56 +0900

    modify comments api url

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom';
 import CommentBox from './comment_box'
 
 ReactDOM.render(
-    <CommentBox url="/api/comments" />,
+    <CommentBox url="http://localhost:3001/api/comments" />,
     document.getElementById('content')
 );
