c0d0ac182a403f8a701798cb65530ec5116f8a76
wusagi24 <lapin.wuusan@gmail.com>
2016-08-14 00:29:47 +0900

    tutorial6.js

--- src/comment.js
+++ src/comment.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
+import Remarkable from 'remarkable';
 
 class Comment extends Component {
     render() {
+        let md = new Remarkable();
         return (
             <div className="commnet">
                 <h2 className="commentAuthor">
                     {this.props.author}
                 </h2>
-                {this.props.children}
+                {md.render(this.props.children.toString())}
             </div>
         );
     }
