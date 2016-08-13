7e531ce8aaf37ded4d9b3d5c2a89913ebce95644
wusagi24 <lapin.wuusan@gmail.com>
2016-08-14 04:57:22 +0900

    tutorial11.js

--- src/index.js
+++ src/index.js
@@ -2,12 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentBox from './comment_box'
 
-let data = [
-    {id: 1, author: "Pete Hunt", text: "This is one comment"},
-    {id: 2, author: "Jordan Walke", text: "This is *author* comment"}
-];
-
 ReactDOM.render(
-    <CommentBox data={data}/>,
+    <CommentBox url="/api/comments" />,
     document.getElementById('content')
 );
