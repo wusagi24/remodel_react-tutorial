7e79b20b9ea9618ecee9c04eb695d48673ad5ede
wusagi24 <lapin.wuusan@gmail.com>
2016-08-14 00:05:37 +0900

    tutorial3.js

--- src/comment_box.js
+++ src/comment_box.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import CommentList from './comment_list';
+import CommentForm from './comment_form';
 
 class CommentBox extends Component {
     render() {
         return (
             <div className="commentBox">
-                Hello, world! I am a CommentBox.
+                <h1>Comments</h1>
+                <CommentList />
+                <CommentForm />
             </div>
         );
     };
