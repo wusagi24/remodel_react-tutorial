3b4c971cb9d55e034a664387d996f57dc33fee71
wusagi24 <lapin.wuusan@gmail.com>
2016-08-14 05:10:40 +0900

    tutorial13.js

--- src/comment_box.js
+++ src/comment_box.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import $ from 'jquery';
 import CommentList from './comment_list';
 import CommentForm from './comment_form';
 
@@ -6,6 +7,20 @@ class CommentBox extends Component {
     constructor(props) {
         super(props);
         this.state = {data: []};
+    }
+
+    componentDidMount() {
+        $.ajax({
+            url: this.props.url,
+            dataType: 'json',
+            cache: false
+        })
+        .done((data) => {
+            this.setState({data: data});
+        })
+        .fail((xhr, status, err) => {
+            console.error(this.props.url, status, err.toString());
+        });
     }
 
     render() {
