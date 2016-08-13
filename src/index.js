ccf8e3011d3799d6eeab06696b5de07e95f71f53
wusagi24 <lapin.wuusan@gmail.com>
2016-08-13 23:48:55 +0900

    tutorial1.js

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import CommentBox from './comment_box'
+
+ReactDOM.render(
+    <CommentBox />,
+    document.getElementById('content')
+);
