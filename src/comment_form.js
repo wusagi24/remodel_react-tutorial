198a6279ffba9335cc2d450417ad6bdbd3b10a49
wusagi24 <lapin.wuusan@gmail.com>
2016-08-14 22:07:36 +0900

    tutorial17.js

--- src/comment_form.js
+++ src/comment_form.js
@@ -14,9 +14,20 @@ class CommentForm extends Component {
         this.setState({text: e.target.value});
     }
 
+    handleSubmit(e) {
+        e.preventDefault();
+        let author = this.state.author.trim();
+        let text = this.state.text.trim();
+        if (!text || !author) {
+            return;
+        }
+        // TODO: サーバーに要求を送信
+        this.setState({author: '', text: ''});
+    }
+
     render() {
         return (
-            <form className="commentForm">
+            <form className="commentForm" onSubmit={this.handleSubmit.bind(this)}>
                 <input
                     type="text"
                     placeholder="Your name"
